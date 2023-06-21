400d6bd3ceaf1a6582f9b8f0defc0677f85a01e3
Filippo Ciarlo <pippoxx98@hotmail.it>
2023-06-21 14:44:51 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var items = document.querySelectorAll('.circle a');
+
+for (var i = 0, l = items.length; i < l; i++) {
+    items[i].style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(3) + "%";
+    items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(3) + "%";
+}
+
+document.querySelector('.menu-button').onclick = function (e) {
+    e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+}
